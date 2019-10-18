761356aae7749b984cb989bd7b38ab0d1e011a93
Jinxyz <xxyaymanyayxx@hotmail.com>
2019-10-18 11:19:05 +0200

    Fredag gissa talet

--- GissaTalet/src/GissaTalet.java
+++ GissaTalet/src/GissaTalet.java
@@ -1,10 +1,38 @@
+/*
+Uppgift: Gissa Talet
+Skapad 17-10-2019
+Dev: Rasmus Gillberg
+*/
 
-
-
+import java.util.Random;
+import javax.swing.*;
 
     public class GissaTalet {
-        public static void main(String[] args){
+        public static void main(String[] args) {
+            int gissa = 0;
+            Random rand = new Random();
+            int n = rand.nextInt(100);
+            int försök = 0;
 
 
+            gissa = Integer.parseInt(JOptionPane.showInputDialog(null, "Gissa talet"));
+            while (gissa != n) {
+
+                if (gissa < n) {
+                    JOptionPane.showMessageDialog(null, "För lågt!");
+                    gissa = Integer.parseInt(JOptionPane.showInputDialog(null, "Välj ett större tal än " + gissa));
+                    försök++;
+                }
+                else if (gissa > n) {
+                    JOptionPane.showMessageDialog(null, "För högt!");
+                 gissa = Integer.parseInt(JOptionPane.showInputDialog(null, "Välj ett mindre tal än " + gissa));
+                 försök++;
+
+                }
+
+                }
+                if (gissa == n) {
+                JOptionPane.showMessageDialog(null, "Du hade rätt! Du behövde " + försök + " försök!");
+            }
         }
-}
+    }
